d690c6e508851852025795163076763ea213c497
Pulluru Sathvik <2400032783@kluniversity.in>
2026-08-30 04:28:52 +0530

    Clean repository: Keep active Node/Express backend and React frontend, remove legacy backend

--- backend-spring/src/main/java/com/tourtec/repository/DestinationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tourtec.repository;
-
-import com.tourtec.entity.DestinationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DestinationRepository extends JpaRepository<DestinationEntity, String> {
-}
--- backend-spring/src/main/java/com/tourtec/repository/SosLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tourtec.repository;
-
-import com.tourtec.entity.SosLogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SosLogRepository extends JpaRepository<SosLogEntity, String> {
-}
